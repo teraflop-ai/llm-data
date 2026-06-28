f1a330d37e0356ba68733487796ee04846acdab2
Enrico Shippole <enricoship@gmail.com>
2026-06-27 22:10:16 -0400

    update html parsing

--- src/llm_data/parsers/html.py
+++ src/llm_data/parsers/html.py
@@ -21,15 +21,26 @@ def resiliparse_extract(html: str) -> str:
 
 
 class ParseHtml:
-    def __init__(self, parser_type="resiliparse"):
+    def __init__(
+        self,
+        input_column: str = "html",
+        output_column: str = "text",
+        parser_type="resiliparse",
+    ):
+        self.input_column = input_column
+        self.output_column = output_column
         self.parser_type = parser_type
 
     def __call__(self, df: DataFrame) -> DataFrame:
         if self.parser_type == "resiliparse":
-            df = df.with_column("cleaned_html", magic_parse(col("html")))
-            df = df.with_column("text", resiliparse_extract(col("cleaned_html")))
+            df = df.with_column("cleaned_html", magic_parse(col(self.input_column)))
+            df = df.with_column(
+                self.output_column, resiliparse_extract(col("cleaned_html"))
+            ).exclude("cleaned_html")
         elif self.parser_type == "trafilatura":
-            df = df.with_column("text", trafilatura_extract(col("html")))
+            df = df.with_column(
+                self.output_column, trafilatura_extract(col(self.input_column))
+            )
         else:
             return ValueError("Selected parser is not available")
         return df
